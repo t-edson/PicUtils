0bde2938bf6a6a32a9bc3e118d40e9f304349b95
Tito Hinostroza <dev_null_1@hotmail.com>
2017-09-06 00:57:29 -0500

    Add files via upload

--- Pic16Devices.pas
+++ Pic16Devices.pas
@@ -28,22 +28,6 @@ begin
    pic.SetMappRAMCom('120-17F:bnk0');
    pic.SetMappRAMCom('1A0-1BF:bnk1, 1F0-1FF:bnk0');
 end;
-procedure SetRAM16F873_74(pic: TPIC16);
-begin
-   pic.SetStatRAM($020, $07F, cs_impleGPR);
-   pic.SetStatRAM($0A0, $0FF, cs_impleGPR);
-   pic.SetMappRAMCom('120-17F:bnk0, 1A0-1FF:bnk1');
-end;
-procedure SetRAM16F876_77(pic: TPIC16);
-begin
-   pic.SetStatRAM($020, $07F, cs_impleGPR);
-   pic.SetStatRAM($0A0, $0EF, cs_impleGPR);
-   pic.SetStatRAM($110, $16F, cs_impleGPR);
-   pic.bank2.GPRStart:=$10;
-   pic.SetStatRAM($190, $1EF, cs_impleGPR);
-   pic.SetMappRAMCom('0F0-0FF:bnk0, 170-17F:bnk0, 1F0-1FF:bnk0');
-   pic.bank3.GPRStart:=$10;
-end;
 
 procedure GetSupportedDevices(list: TStrings);
 {Devuelve la lista de dispositivos soportados. Esta lista debe ser la misma que hay en
@@ -98,7 +82,8 @@ begin
      pic.NumBanks:=2;
      pic.NumPages:=1; pic.MaxFlash:=1024;  //banco 0 implementado parcialmente
      pic.GPRStart:=$20;
-     pic.SetStatRAM($20, $5F, cs_impleGPR);
+     pic.SetStatRAMCom('000-01F:SFR, 020-05F:GPR');
+     pic.SetStatRAMCom('080-09F:SFR, 0A0-0DF:GPR');
      pic.SetMappRAMCom('0A0-0DF:bnk0');
    end;
    'PIC16C63',
@@ -108,8 +93,8 @@ begin
      pic.NumBanks:=2;
      pic.NumPages:=2; pic.MaxFlash:=4096;
      pic.GPRStart:=$20;
-     pic.SetStatRAM($020, $07F, cs_impleGPR);
-     pic.SetStatRAM($0A0, $0FF, cs_impleGPR);
+     pic.SetStatRAMCom('000-01F:SFR, 020-07F:GPR');
+     pic.SetStatRAMCom('080-09F:SFR, 0A0-0FF:GPR');
    end;
    'PIC16C65',
    'PIC16C65A',
@@ -119,8 +104,8 @@ begin
      pic.NumBanks:=2;
      pic.NumPages:=2; pic.MaxFlash:=4096;
      pic.GPRStart:=$20;
-     pic.SetStatRAM($020, $07F, cs_impleGPR);
-     pic.SetStatRAM($0A0, $0FF, cs_impleGPR);
+     pic.SetStatRAMCom('000-01F:SFR, 020-07F:GPR');
+     pic.SetStatRAMCom('080-09F:SFR, 0A0-0FF:GPR');
    end;
    'PIC16F72': begin
      pic.MaxFreq:=20000000;
@@ -130,7 +115,6 @@ begin
      pic.GPRStart:=$20;
      pic.SetStatRAM($020, $07F, cs_impleGPR);
      pic.SetStatRAM($0A0, $0BF, cs_impleGPR);
-
      pic.SetMappRAMCom('0C0-0FF:bnk0');
      pic.SetMappRAMCom('120-17F:bnk0');
      pic.SetMappRAMCom('1A0-1BF:bnk1, 1C0-1FF:bnk0');
@@ -197,7 +181,11 @@ begin
      pic.NumBanks:=4;    //los bancos 2 y 3 están reflejados
      pic.NumPages:=2; pic.MaxFlash:=4096;
      pic.GPRStart:=$20;
-     SetRAM16F873_74(pic);
+     pic.SetStatRAMCom('000-01F:SFR, 020-07F:GPR');
+     pic.SetStatRAMCom('080-09F:SFR, 0A0-0FF:GPR');
+     pic.SetStatRAMCom('100-10F:SFR, 120-17F:GPR');
+     pic.SetStatRAMCom('180-18F:SFR, 1A0-1FF:GPR');
+     pic.SetMappRAMCom('120-17F:bnk0, 1A0-1FF:bnk1');
    end;
    'PIC16F874',
    'PIC16F874A': begin
@@ -206,7 +194,11 @@ begin
      pic.NumBanks:=4;    //los bancos 2 y 3 están reflejados
      pic.NumPages:=2; pic.MaxFlash:=4096;
      pic.GPRStart:=$20;
-     SetRAM16F873_74(pic);
+     pic.SetStatRAMCom('000-01F:SFR, 020-07F:GPR');
+     pic.SetStatRAMCom('080-09F:SFR, 0A0-0FF:GPR');
+     pic.SetStatRAMCom('100-10F:SFR, 120-17F:GPR');
+     pic.SetStatRAMCom('180-18F:SFR, 1A0-1FF:GPR');
+     pic.SetMappRAMCom('120-17F:bnk0, 1A0-1FF:bnk1');
    end;
    'PIC16F876',
    'PIC16F876A': begin
@@ -215,7 +207,13 @@ begin
      pic.NumBanks:=4;
      pic.NumPages:=4; pic.MaxFlash:=8192;
      pic.GPRStart:=$20;   //es solo el valor de los bancoa 0 y 1
-     SetRAM16F876_77(pic);
+     pic.bank2.GPRStart:=$10;
+     pic.bank3.GPRStart:=$10;
+     pic.SetStatRAMCom('000-01F:SFR, 020-07F:GPR');
+     pic.SetStatRAMCom('080-09F:SFR, 0A0-0FF:GPR');
+     pic.SetStatRAMCom('100-10F:SFR, 110-17F:GPR');
+     pic.SetStatRAMCom('180-18F:SFR, 190-1FF:GPR');
+     pic.SetMappRAMCom('0F0-0FF:bnk0, 170-17F:bnk0, 1F0-1FF:bnk0');
    end;
    'PIC16F877',
    'PIC16F877A',
@@ -225,7 +223,13 @@ begin
      pic.NumBanks:=4;
      pic.NumPages:=4; pic.MaxFlash:=8192;
      pic.GPRStart:=$20;   //es solo el valor de los bancoa 0 y 1
-     SetRAM16F876_77(pic);
+     pic.bank2.GPRStart:=$10;
+     pic.bank3.GPRStart:=$10;
+     pic.SetStatRAMCom('000-01F:SFR, 020-07F:GPR');
+     pic.SetStatRAMCom('080-09F:SFR, 0A0-0FF:GPR');
+     pic.SetStatRAMCom('100-10F:SFR, 110-17F:GPR');
+     pic.SetStatRAMCom('180-18F:SFR, 190-1FF:GPR');
+     pic.SetMappRAMCom('0F0-0FF:bnk0, 170-17F:bnk0, 1F0-1FF:bnk0');
    end;
    'PIC16F627A': begin
      pic.MaxFreq:=20000000;
